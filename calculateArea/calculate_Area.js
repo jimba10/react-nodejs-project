ff28fea21a21f6f563bc74fb83e500830943d4ee
banji <banjiadeogun@gmail.com>
2026-04-19 13:44:46 -0400

    I updated the ControlArea folder

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,8 +1,9 @@
 let length;
 let width;
 function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
-    let area = length * width;
-    document.getElementById('result').innerText = 'The area of a triangle is ${area}';
+ length = parseFloat(document.getElementById('length').value);
+ width = parseFloat(document.getElementById('width').value);
+
+ let area = length * width; 
+ document.getElementById('result').innerText = `The area of a triangle is: ${area}`;   
 }
